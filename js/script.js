d77fac3da63de71f65bd42fe380ee29a9e6ef578
Justin Woodward <j_wooo@hotmail.com>
2016-10-22 17:35:53 -0400

    password protect for now

--- js/script.js
+++ js/script.js
@@ -1,18 +1,40 @@
 $(document).ready(function() {
 
+	var count = 0;
+
+	$('#not-done').on('click', function() {
+
+		var phrase = ['lol..wrong!', 'Cant guess it?', 'Dude. Just stop...', 'hahahahahahahahaaaa........stop.', 'Dude. Fail one more time and I will lock you out...', 'GOT YOU! No really I am serious this time..', 'last chance...', 'Buh-Bye!'];
+
+		if ($('#password-check').val() === 'jw') {
+			$('#finish').hide();
+		} else {
+			$('.lol').html(phrase[count]);
+		}
+
+		count++;
+
+		if(count >= 8) {
+			setTimeout(function() {
+				$('.finish-box').html(':)');
+			},3000);
+		}
+
+	});
+
 	var event = 'None at this time.';
 	var mq = window.matchMedia( "(max-width: 991px)" );
+	var mq2 = window.matchMedia( "(max-width: 767px)" );
 
 	if (mq.matches) {
 
 		$('form').removeClass('forms');
 		$('.form-group').addClass('sm-12');
 		$('nav').addClass('navbar-fixed-top');
+	}
 
-		if ($('.index-footer').length) {
-			$('#slideshow').addClass('index-margin');
-		}
-	
+	if (mq2.matches) {
+		$('.navbar-brand').html('<img class="img-responsive" alt="brand" src="img/footerlogo.png">');
 	}
 
 	//Populates upcoming events section in footer.
