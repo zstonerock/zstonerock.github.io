7b4bf0d7794f1ddb05f6bd77ac6ada3bed678be2
Justin Woodward <j_wooo@hotmail.com>
2016-10-25 12:40:59 -0400

    Debug commit
    study for any and all bugs needing addressed before proper launch to the
    publicly addressed audience.

--- js/script.js
+++ js/script.js
@@ -1,27 +1,4 @@
 $(document).ready(function() {
-
-	var count = 0;
-
-	$('#not-done').on('click', function() {
-
-		var phrase = ['lol..wrong!', 'Cant guess it?', 'Dude. Just stop...', 'hahahahahahahahaaaa........stop.', 'Dude. Fail one more time and I will lock you out...', 'GOT YOU! No really I am serious this time..', 'last chance...', 'Buh-Bye!'];
-
-		if ($('#password-check').val() === 'jw') {
-			$('#finish').hide();
-		} else {
-			$('.lol').html(phrase[count]);
-		}
-
-		count++;
-
-		if(count >= 8) {
-			setTimeout(function() {
-				$('.finish-box').html(':)');
-			},3000);
-		}
-
-	});
-
 	var event = 'None at this time.';
 	var mq = window.matchMedia( "(max-width: 991px)" );
 	var mq2 = window.matchMedia( "(max-width: 767px)" );
@@ -39,5 +16,4 @@ $(document).ready(function() {
 
 	//Populates upcoming events section in footer.
 	$('#events').html(event);
-
 }); // end doc ready
